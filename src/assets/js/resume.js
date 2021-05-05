e7094dd426ae316bb1007029488c288b991424ae
ngocngo <nguyenngoc.rnd@gmail.com>
2021-05-05 16:47:08 +0700

    edit extra info

--- src/assets/js/resume.js
+++ src/assets/js/resume.js
@@ -9,6 +9,14 @@ var btn_edit_education = document.querySelector('#btn-edit-education');
 var edit_skills = document.querySelectorAll('.edit-skill');
 var show_skills = document.querySelectorAll('.show-skill');
 var btn_edit_skill = document.querySelector('#btn-edit-skill');
+
+var edit_score = document.querySelectorAll('.edit-score');
+var show_score = document.querySelectorAll('.show-score');
+var btn_edit_score = document.querySelector('#btn-edit-score');
+
+var edit_extra= document.querySelectorAll('.edit-extra');
+var show_extra = document.querySelectorAll('.show-extra');
+var btn_edit_extra = document.querySelector('#btn-edit-extra');
 // window.addEventListener('load', (event) => {
 //     //console.log('The page has fully loaded');
 //     //hideElement(edit_experiences);
@@ -49,4 +57,14 @@ function editSkill() {
     showHideElement(edit_skills);
     showHideElement(show_skills);
     toggleButtonLabel(btn_edit_skill);
+}
+function editScore() {
+    showHideElement(edit_score);
+    showHideElement(show_score);
+    toggleButtonLabel(btn_edit_score);
+}
+function editExtraInfo() {
+    showHideElement(edit_extra);
+    showHideElement(show_extra);
+    toggleButtonLabel(btn_edit_extra);
 }
