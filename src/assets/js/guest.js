5af6cf14b2017111ebbf2a8c5a3d56dc468180d9
Dennis Tran <dennis@infinityels.com>
2021-05-10 22:39:29 +0700

    resolved some bugs on login/signup page

--- src/assets/js/guest.js
+++ src/assets/js/guest.js
@@ -1,28 +1,28 @@
-const signUpToggle = new PasswordToggle ('.new-password',{
+const signUpToggle = new PasswordToggle('.new-password', {
     onOn: (btn) => {
         btn.classList.add('btn');
-        btn.innerHTML = '<i class="fas fa-eye-slash"></i>';
+        btn.innerHTML = '<i class="fa fa-eye-slash"></i>';
     },
     onOff: (btn) => {
         btn.classList.add('btn');
-        btn.innerHTML = '<i class="fas fa-eye"></i>';
+        btn.innerHTML = '<i class="fa fa-eye"></i>';
     }
 });
 
-const loginToggle = new PasswordToggle ('.current-password',{
+const loginToggle = new PasswordToggle('.current-password', {
     onOn: (btn) => {
         btn.classList.add('btn');
-        btn.innerHTML = '<i class="fas fa-eye-slash"></i>';
+        btn.innerHTML = '<i class="fa fa-eye-slash"></i>';
     },
     onOff: (btn) => {
         btn.classList.add('btn');
-        btn.innerHTML = '<i class="fas fa-eye"></i>';
+        btn.innerHTML = '<i class="fa fa-eye"></i>';
     }
 });
 
-const forms = document.querySelectorAll('.signupform');
+const forms = document.querySelectorAll('.signup-form');
 forms.forEach(form => {
-    form.addEventListener('submit', function(){
+    form.addEventListener('submit', function () {
         form.signupbtn.disabled = true;
     });
 });
